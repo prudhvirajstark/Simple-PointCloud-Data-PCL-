fca37330e5953d987c77c56736f6019d89e2773c
prudhvirajstark <panisettiprudhviraj@gmail.com>
2021-06-13 15:15:41 +0200

    Added Build folder for Simple Hello world program with CMake.

---
+++ build/CMakeFiles/Simple-PointCloud-Data-PCL.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Simple-PointCloud-Data-PCL.dir/src/main.cpp.obj"
+  "CMakeFiles/Simple-PointCloud-Data-PCL.dir/src/main.cpp.obj.d"
+  "Simple-PointCloud-Data-PCL.exe"
+  "Simple-PointCloud-Data-PCL.exe.manifest"
+  "Simple-PointCloud-Data-PCL.pdb"
+  "libSimple-PointCloud-Data-PCL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Simple-PointCloud-Data-PCL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Simple-PointCloud-Data-PCL.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Simple-PointCloud-Data-PCL.
+# This may be replaced when dependencies are built.
